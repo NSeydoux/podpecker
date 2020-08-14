de7e56733d252b14f6a6c7c7322dab768193b996
Nicolas Seydoux <nseydoux@inrupt.com>
2020-08-14 19:03:33 +0200

    Set up a basic fetcher
    This sets up a page allowing the user to log in and to fetch some data.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,97 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import {
+  Session as AuthSession,
+  getClientAuthenticationWithDependencies
+} from "@inrupt/solid-client-authn-browser"
 
 export default function Home() {
+  const [session, setSession] = useState(new AuthSession(
+    {
+      clientAuthentication: getClientAuthenticationWithDependencies({})
+    },
+    "mySession"
+  ));
+  const [issuer, setIssuer] = useState("https://broker.demo-ess.inrupt.com/");
+  const [oidc, setOidc] = useState("awaiting_login");
+  const [resource, setResource] = useState(session.info.webId);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if(oidc === "login_sent") {
+      session.login({
+        redirectUrl: new URL("http://localhost:3000/"),
+        oidcIssuer: new URL(issuer)
+      });
+    }
+  }, [oidc]);
+
+  useEffect(() => {
+    const authCode =
+        new URL(window.location.href).searchParams.get("code");
+    if (authCode) {
+      console.log("Being redirected from the IdP")
+      session.handleIncomingRedirect(
+        new URL(window.location.href)
+      ).then((info) => {
+        setResource(info.webId)
+      });
+    }
+  }, []);
+
+  const handleLogin = (e) => {
+    // The default behaviour of the button is to resubmit. 
+    // This prevents the page from reloading.
+    e.preventDefault();
+    setOidc("login_sent")
+  }
+
+  const handleFetch = (e) => {
+    // The default behaviour of the button is to resubmit. 
+    // This prevents the page from reloading.
+    e.preventDefault();
+    session.fetch(resource).then(response => response.text()).then(setData);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>PodPecker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Peck some resources into your Pod
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <p>{session.info.webId ? `Logged in as ${session.info.webId}` : "Not logged in yet"} </p>
+        <div>
+        <form>
+          <input
+            type="text"
+            value={issuer}
+            onChange={e => {
+              setIssuer(e.target.value);
+            }}
+          />
+          <button onClick={(e) => handleLogin(e)}>Log In</button>
+        </form>
         </div>
+        <div>
+        <input
+            type="text"
+            value={resource}
+            onChange={e => {
+              setResource(e.target.value);
+            }}
+          />
+          <button onClick={(e) => handleFetch(e)}>Fetch</button>
+        </div>
+        <pre>
+          {data}
+        </pre>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
