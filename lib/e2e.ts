f0bf7bdd53d9bbd00aa8bd720ccbcfc923bac173
Nicolas Seydoux <nseydoux@inrupt.com>
2020-08-18 15:08:07 +0200

    Create test data conformant to e2e expectations

--- lib/e2e.ts
+++ lib/e2e.ts
@@ -1,4 +1,4 @@
-import { createFile, makePublic, createContainer, writeSomeData, deleteTarget } from "./utlis";
+import { createFile, makePublic, createContainer, writeSomeData, deleteTarget, giveResourceAccessTo, giveDefaultAccessTo } from "./utlis";
 import { Session } from "@inrupt/solid-client-authn-browser";
 
 export async function initE2eTests(session: Session, rootContainer: string, owner: string, testSlug: string) {
@@ -33,40 +33,75 @@ export async function initE2eTests(session: Session, rootContainer: string, owne
 
     iri = await createContainer(session, testRoot, owner, "lit-pod-acl-test");
     await makePublic(session, iri);
-    iri = await createContainer(session, iri, owner, "passthrough-container");
-    await makePublic(session, iri);
+    await giveDefaultAccessTo(session, iri, "https://vincentt.inrupt.net/profile/card#me", {
+        read: true, 
+        write: false, 
+        control: false,
+        append: false
+    })
+    iri = await createContainer(session, iri, owner, "passthrough-container", false);
+    // await makePublic(session, iri);
     await makePublic(session, await writeSomeData(session, iri, owner, "", "resource-with-acl.ttl", true));
+    await giveResourceAccessTo(session, `${iri}resource-with-acl.ttl`, "https://vincentt.inrupt.net/profile/card#me", {
+        read: false, 
+        write: false, 
+        control: false,
+        append: true
+    })
     // The following resource should be public by default
     await writeSomeData(session, iri, owner, "", "resource-without-acl.ttl", false);
-    console.log("lit-pod-acl-initialisation-test created")
+    console.log("lit-pod-acl-test created")
 
     iri = await createContainer(session, testRoot, owner, "lit-solid-core-resource-info-test");
     await makePublic(session, iri);
     await makePublic(session, await writeSomeData(session, iri, owner, "", "litdataset.ttl", true));
-    await makePublic(session, await createFile(session, `${iri}not-a-lit-dataset.png`, owner, "test text", "text/plain"));
+    await makePublic(session, await createFile(session, `${iri}not-a-litdataset.png`, owner, "test text", "text/plain"));
     console.log("lit-solid-core-resource-info-test created");
 
-    await makePublic(session, await writeSomeData(session, testRoot, owner, "", "lit-solid-core-test.ttl", true));
+    iri = await createContainer(session, testRoot, owner, "lit-pod-resource-info-test");
+    await makePublic(session, iri);
+    await makePublic(session, await writeSomeData(session, iri, owner, "", "litdataset.ttl", true));
+    await makePublic(session, await createFile(session, `${iri}not-a-litdataset.png`, owner, "test text", "text/plain"));
+    console.log("lit-pod-resource-info-test created");
+
+    const someData = `@prefix : <#>.
+    @prefix schema: <http://schema.org/>.
+    @prefix n0: <http://xmlns.com/foaf/0.1/>.
+    @prefix XML: <http://www.w3.org/2001/XMLSchema#>.
+    
+    :thing1
+        a n0:Document;
+        n0:name "Thing for first end-to-end test";
+        n0:nick "Random nick 0.09692964405333937".`
+
+    await makePublic(session, await writeSomeData(session, testRoot, owner, someData, "lit-solid-core-test.ttl", true));
+    await makePublic(session, await writeSomeData(session, testRoot, owner, someData, "lit-pod-test.ttl", true));
     console.log("lit-solid-core-test.ttl created");
 }
 
 export async function cleanupTestData(session: Session, rootContainer: string) {
-    await deleteTarget(session, `${rootContainer}lit-solid-core-test.ttl`);
-    
-    await deleteTarget(session, `${rootContainer}lit-solid-core-resource-info-test/litdataset.ttl`);
-    await deleteTarget(session, `${rootContainer}lit-solid-core-resource-info-test/not-a-lit-dataset.png`);
-    await deleteTarget(session, `${rootContainer}lit-solid-core-resource-info-test/`);
+    await deleteTarget(session, `${rootContainer}arbitrary.json`);
+
+    await deleteTarget(session, `${rootContainer}container-test/`);
+
+    await deleteTarget(session, `${rootContainer}lit-pod-acl-initialisation-test/resource.ttl`);
+    await deleteTarget(session, `${rootContainer}lit-pod-acl-initialisation-test/`);
 
     await deleteTarget(session, `${rootContainer}lit-pod-acl-test/passthrough-container/resource-with-acl.ttl`);
     await deleteTarget(session, `${rootContainer}lit-pod-acl-test/passthrough-container/resource-without-acl.ttl`);
     await deleteTarget(session, `${rootContainer}lit-pod-acl-test/passthrough-container/`);
     await deleteTarget(session, `${rootContainer}lit-pod-acl-test/`);
 
-    await deleteTarget(session, `${rootContainer}lit-pod-acl-initialisation-test/resource.ttl`);
-    await deleteTarget(session, `${rootContainer}lit-pod-acl-initialisation-test/`);
+    await deleteTarget(session, `${rootContainer}lit-solid-core-resource-info-test/litdataset.ttl`);
+    await deleteTarget(session, `${rootContainer}lit-solid-core-resource-info-test/not-a-litdataset.png`);
+    await deleteTarget(session, `${rootContainer}lit-solid-core-resource-info-test/`);
 
-    await deleteTarget(session, `${rootContainer}container-test/`);
+    await deleteTarget(session, `${rootContainer}lit-pod-resource-info-test/litdataset.ttl`);
+    await deleteTarget(session, `${rootContainer}lit-pod-resource-info-test/not-a-litdataset.png`);
+    await deleteTarget(session, `${rootContainer}lit-pod-resource-info-test/`);
 
-    await deleteTarget(session, `${rootContainer}arbitrary.json`);
+    await deleteTarget(session, `${rootContainer}lit-solid-core-test.ttl`);
+    await deleteTarget(session, `${rootContainer}lit-pod-test.ttl`);
+ 
     await deleteTarget(session, `${rootContainer}`);
 }
--- lib/utlis.ts
+++ lib/utlis.ts
@@ -30,7 +30,8 @@ import {
     asIri,
     asUrl,
     ThingPersisted,
-    deleteFile
+    deleteFile,
+    setAgentDefaultAccess
  } from "@inrupt/solid-client";
 
  import {
@@ -112,6 +113,75 @@ export async function makePublic(session: Session, resourceIri: string) {
     await saveAclFor(myDatasetWithAcl, updatedAcl, { fetch: session.fetch });
 }
 
+export async function giveResourceAccessTo(session: Session, resourceIri: string, agent: string, access: {read: boolean, write: boolean, append: boolean, control: boolean}) {
+    console.log(`Giving some access to ${agent} for ${resourceIri}`);
+    const myDatasetWithAcl = await getSolidDatasetWithAcl(resourceIri, { fetch: session.fetch });
+    let resourceAcl;
+    if (!hasResourceAcl(myDatasetWithAcl)) {
+        if (!hasAccessibleAcl(myDatasetWithAcl)) {
+            throw new Error(
+            "The current user does not have permission to change access rights to this Resource."
+            );
+        }
+        if (!hasFallbackAcl(myDatasetWithAcl)) {
+            throw new Error(
+            "The current user does not have permission to see who currently has access to this Resource."
+            );
+        }
+        resourceAcl = createAclFromFallbackAcl(myDatasetWithAcl);
+    } else {
+        resourceAcl = getResourceAcl(myDatasetWithAcl);
+    }
+
+    // Set the access
+    let updatedAcl = setAgentResourceAccess(
+        resourceAcl,
+        agent,
+        access,
+    );
+    await saveAclFor(myDatasetWithAcl, updatedAcl, { fetch: session.fetch });
+}
+
+export async function giveDefaultAccessTo(session: Session, resourceIri: string, agent: string, access: {read: boolean, write: boolean, append: boolean, control: boolean}) {
+    console.log(`Giving default access to ${agent} for ${resourceIri}`);
+    const myDatasetWithAcl = await getSolidDatasetWithAcl(resourceIri, { fetch: session.fetch });
+    let resourceAcl;
+    if (!hasResourceAcl(myDatasetWithAcl)) {
+        if (!hasAccessibleAcl(myDatasetWithAcl)) {
+            throw new Error(
+            "The current user does not have permission to change access rights to this Resource."
+            );
+        }
+        if (!hasFallbackAcl(myDatasetWithAcl)) {
+            throw new Error(
+            "The current user does not have permission to see who currently has access to this Resource."
+            );
+        }
+        resourceAcl = createAclFromFallbackAcl(myDatasetWithAcl);
+    } else {
+        resourceAcl = getResourceAcl(myDatasetWithAcl);
+    }
+
+    // Make the resource public
+    let updatedAcl = setAgentDefaultAccess(
+        resourceAcl,
+        agent,
+        access,
+    );
+    // On ESS, default access requires resource access
+    getThingAll(updatedAcl).forEach(thing => {
+        if (getIri(thing, "http://www.w3.org/ns/auth/acl#default")) {
+            const newthing = addIri(
+                thing,
+                "http://www.w3.org/ns/auth/acl#accessTo",
+                getIri(thing, "http://www.w3.org/ns/auth/acl#default")
+            );
+            updatedAcl = setThing(updatedAcl, newthing);
+        }
+    })
+    await saveAclFor(myDatasetWithAcl, updatedAcl, { fetch: session.fetch });
+}
+
 async function isTurtle(turtle: string, targetIri: string): Promise<boolean> {
     return turtleToTriples(turtle, targetIri).then(() => true).catch(() => false);
 }
@@ -145,7 +215,8 @@ export async function createContainer(
         session: Session, 
         rootContainer: string, 
         owner: string, 
-        slug: string) {
+        slug: string, 
+        setAcl: boolean = true) {
     const containerIri = `${rootContainer}${slug}/`
     await session.fetch(rootContainer, {
         method: "POST",
@@ -155,6 +226,9 @@ export async function createContainer(
             "Slug": slug
         }
     })
+    if (!setAcl) {
+        return containerIri;
+    }
     console.log(`${containerIri} created, setting access`);
     // Get the ACL for the created file
     const myDatasetWithAcl = await getSolidDatasetWithAcl(
